92674cca9a301ec825bf1e539b54708ccad2388b
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2025-03-11 11:03:12 +0000

    Made sure mobile users can be anonymous too

--- nextjs-frontend/src/components/event/components/VerticalDashboard/WeekEvents.tsx
+++ nextjs-frontend/src/components/event/components/VerticalDashboard/WeekEvents.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {DaysType, KeystoneEvent} from "@/components/event/types/event";
 import {getDays} from "@/lib/date";
 import {WeekEventList} from "@/components/global/styles/ItemStyles";
-import {useUser} from "@/components/user-authentication/hooks/useUser";
 import {Accordion} from "@/components/event/components/VerticalDashboard/Accordion";
 
 interface ListingProps {
@@ -10,10 +9,6 @@ interface ListingProps {
 }
 
 export const WeekEvents: React.FC<ListingProps> = ({events}: ListingProps) => {
-    const user = useUser()
-
-    if (!user) return null
-
     return (<>
             <WeekEventList className="accordion">
                 {getDays().map((day: DaysType) => {
